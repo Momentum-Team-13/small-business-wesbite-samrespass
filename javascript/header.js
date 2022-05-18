e148ecd74d8b1094fea0f51a3708e2021b3934b5
Sam Respass <samrespass@gmail.com>
2022-05-18 11:21:48 -0400

    moving dummy's files to right location

---
+++ javascript/header.js
@@ -0,0 +1,5 @@
+//I'm literally looking at a site I made a few months ago and copying the very basic javascript in there to look more impressive to my peers
+//Oohhoh lookit the college boy over here
+//anyways here's my basic header to call on each page
+//truth be told I wanted to try making my own js file teehee
+document.querySelector(".plantHeader").innerHTML = '<header align="center"><div id="nav"><nav class="main-nav"><a href="index.html" class="nav-link">Home</a> <a href="in-stock.html" class="nav-link">In Stock</a>  <a href="info.html" class="nav-link">Store Hours & Location</a></nav></div></header>'
